a10eefebae953886f5637c4719cfce100077b5e0
SharmaDevanshu089 <sharma.devanshu089@gmail.com>
2026-02-13 15:21:10 +0530

    Initial Commit

---
+++ src-tauri/src/main.rs
@@ -0,0 +1,8 @@
+// Prevents additional console window on Windows in release, DO NOT REMOVE!!
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+fn main() {
+    //will only run tui until gui is called explicitly
+    //refresh_rate_helper_lib::run()
+    println!("Hello World");
+}
