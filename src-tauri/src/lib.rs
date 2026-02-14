5f4c94ac8997b2d8e00e510133187b255702f68d
SharmaDevanshu089 <sharma.devanshu089@gmail.com>
2026-02-14 14:19:34 +0530

    Added Funtion to Check Display Settings

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,7 +1,16 @@
 use tauri::menu::Menu;
 use tauri::menu::MenuItem;
 use tauri::tray::TrayIconBuilder;
+use windows::core::*;
+use windows::Win32::Graphics::Gdi::*;
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+#[derive(Debug, serde::Serialize, Clone)]
+pub struct CurrentDisplayInfo {
+    pub width: u32,
+    pub height: u32,
+    pub refresh_rate: u32,
+    pub bit_rate: u32,
+}
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
@@ -16,7 +25,7 @@ pub fn run() {
             let create_window_menu =
                 MenuItem::with_id(app, "create_gui_window", "Options", true, None::<&str>)?;
             let tray_menu = Menu::with_items(app, &[&create_window_menu, &menu_quit])?;
-            let tray = TrayIconBuilder::new()
+            let _tray = TrayIconBuilder::new()
                 .menu(&tray_menu)
                 .icon(app.default_window_icon().unwrap().clone())
                 .build(app)?;
@@ -26,4 +35,30 @@ pub fn run() {
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
+}
+
+pub fn get_current_display() -> std::result::Result<CurrentDisplayInfo, String> {
+    unsafe {
+        let device_name = w!("\\\\.\\DISPLAY1");
+        let mut dev_mode = DEVMODEW::default();
+        dev_mode.dmSize = std::mem::size_of::<DEVMODEW>() as u16;
+        let setting_boolean = EnumDisplaySettingsExW(
+            PCWSTR(device_name.as_ptr()),
+            ENUM_CURRENT_SETTINGS,
+            &mut dev_mode,
+            ENUM_DISPLAY_SETTINGS_FLAGS(0),
+        )
+        .as_bool();
+
+        if setting_boolean {
+            Ok(CurrentDisplayInfo {
+                width: dev_mode.dmPelsWidth,
+                height: dev_mode.dmPelsHeight,
+                refresh_rate: dev_mode.dmDisplayFrequency,
+                bit_rate: dev_mode.dmBitsPerPel,
+            })
+        } else {
+            Err("Failed to Read Current Display".to_string())
+        }
+    }
 }
